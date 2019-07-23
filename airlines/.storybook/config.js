73d58a6ceec844a4ba0dbf6c69c4a9e14377bd81
Julita Karolczuk <julitka015@wp.pl>
2019-07-23 23:19:52 +0200

    added addons to storybook

--- airlines/.storybook/config.js
+++ airlines/.storybook/config.js
@@ -1,10 +1,12 @@
-import { configure } from '@storybook/react'
+import { configure, addParameters } from '@storybook/react'
 import '../src/airlines/airlines.css'
 import '../src/search/reservations.css'
 import '../src/App.css'
 
+addParameters({ viewport: 'iphone6'});
+
 const loadStories = () => {
-  require('../src/stories/index.js')
+  // require('../src/stories/index.js')
   require('../src/stories/search.js')
   require('../src/stories/filghts.js')
   require('../src/stories/header.js')
--- airlines/src/stories/index.js
+++ airlines/src/stories/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'
 import { Button } from '@storybook/react/demo';
 import InputField from '../common/InputField';
+import { withKnobs, text, boolean, number, withActions } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('TEST', module)
+  .addDecorator(withKnobs)
+  .add('as dynamic variables', () => {
+    const groupId = 'GROUP-ID1'
+    const name = text('Name', 'Arunoda Susiripala', groupId);
+    const shouldDisplay = boolean(true, groupId);
+    const numberTest = number(4, groupId);
+
+    const content = `My name is ${name}.`;
+    return shouldDisplay ? (<div>{content}{numberTest}</div>) : <p>ASD</p>;
+  })
+  .addDecorator(withActions('mouseover', 'asd'))
   .add('with text', () => (
     <Button>Hello Button</Button>
   ))
   .add('with emoji', () => (
-    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+    <Button onClick={action('asd')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
   ))
   .add('with test input', () => (
     <InputField meta={{ touched: true }} />
